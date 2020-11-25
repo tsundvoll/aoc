937b9b7c43c3761553edc134f9b6985b29b257d9
Thomas Sundvoll <thomas@sundvoll.net>
2020-11-25 16:06:21 +0100

    Add template folder

---
+++ aoe_2019/template_day/solution.py
@@ -0,0 +1,14 @@
+data = [x for x in open('input.txt').read().splitlines()]
+
+
+def solution_part_1():
+    return "Not solved yet"
+
+
+def solution_part_2():
+    return "Not solved yet"
+
+
+if __name__ == "__main__":
+    print("Answer Part 1:", solution_part_1())
+    print("Answer Part 2:", solution_part_2())
---
+++ aoe_2019/template_day/tests.py
@@ -0,0 +1,11 @@
+from solution import *
+import pytest
+
+@pytest.mark.skip("Not implemented yet")
+def test_part_1():
+    raise NotImplementedError
+
+
+@pytest.mark.skip("Not implemented yet")
+def test_part_2():
+    raise NotImplementedError
