ae705469838c6004c1ff298ba36be7a3a8052a48
Thomas Sundvoll <thomas@sundvoll.net>
2022-12-01 05:48:52 +0100

    Setup for 2022

---
+++ aoc_2022/template/tests.py
@@ -0,0 +1,30 @@
+import pytest
+from solution import first_task, second_task
+
+
+@pytest.mark.parametrize(
+    "input, expected_output",
+    [
+        (
+            """example_input""",
+            0
+        ),
+    ]
+)
+def test_first_task(input, expected_output):
+    ex = list(map(lambda l: l.strip(), input.splitlines()))
+    assert first_task(ex) == expected_output
+
+
+@pytest.mark.parametrize(
+    "input, expected_output",
+    [
+        (
+            """example_input""",
+            None
+        ),
+    ]
+)
+def test_second_task(input, expected_output):
+    ex = list(map(lambda l: l.strip(), input.splitlines()))
+    assert second_task(ex) == expected_output
