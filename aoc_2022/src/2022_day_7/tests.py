b6ae086e072115f6e5896fe0dd24aa357863b3ef
Thomas Sundvoll <thomas@sundvoll.net>
2022-12-07 07:57:24 +0100

    Solve day 7 2022

---
+++ aoc_2022/src/2022_day_7/tests.py
@@ -0,0 +1,76 @@
+import pytest
+from solution import first_task, second_task
+
+
+@pytest.mark.parametrize(
+    "input, expected_output",
+    [
+        (
+            """$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k
+""",
+            95437,
+        ),
+    ],
+)
+def test_first_task(input, expected_output):
+    ex = list(map(lambda l: l.strip(), input.splitlines()))
+    assert first_task(ex) == expected_output
+
+
+@pytest.mark.parametrize(
+    "input, expected_output",
+    [
+        (
+            """$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k
+""",
+            24933642,
+        ),
+    ],
+)
+def test_second_task(input, expected_output):
+    ex = list(map(lambda l: l.strip(), input.splitlines()))
+    assert second_task(ex) == expected_output
